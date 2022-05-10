17b2327e38fea40e38679d4714a3805cc210a8e0
Michael Kleyman <100313713+MichaelKleyman@users.noreply.github.com>
2022-05-10 19:33:51 -0400

    main file

---
+++ main.py
@@ -0,0 +1,19 @@
+import datetime
+import smtplib
+import random
+
+my_email = "sdeveloper444@gmail.com"
+password = "206480220aB"
+
+now = datetime.datetime.now()
+if now.weekday() == 1:
+    with open("Birthday Wisher (Day 32) start/quotes.txt") as data:
+        quote_list = data.readlines()
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="michaelkleyman99@gmail.com",
+                            msg=f"Subject:Weekly Motivation\n\n{random.choice(quote_list)}\nSincerely Michael Kleyman")
+
+#
